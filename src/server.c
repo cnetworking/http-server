c5edfcf36a2c14aa7cab92cf20b9fced0487aa28
xoreo <mattnappo@gmail.com>
2018-12-02 20:08:01 -0500

    writing server socket and init http header

--- src/server.c
+++ src/server.c
@@ -1,18 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
 
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 #include "http_server.h"
 
 int main(int argc, char *argv[]) {
-	printf("server here\n");
-
+    // Open the index.html file
     FILE *html_data;
-    html_data = fopen("./static/index.html", "r");
-    printf("len: %d\n", file_count("./static/index.html"));
-    printf("len: %d\n", file_count("client.c"));
+    html_data = fopen("./src/static/index.html", "r");
+    
+    // Read from the file
+    char response_data[1024];
+    fgets(response_data, sizeof(response_data), html_data);
 
+    // Initialize the http header
+    char http_header[2048] = "HTTP/1.0 200 OK\r\n\n";
+    strcat(http_header, response_data);
+
+    // Create the server socket
+    int server_socket;
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    
+    // Define the socket address
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(3000);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind the socket to the network
+    bind(
+      server_socket,
+      (struct sockaddr *) &server_address,
+      sizeof(server_address)
+    );
+    
+    // Start listening to connections
+    printf("awaiting connections\n");
+    listen(server_socket, MAX_CLIENTS);
+
+    return 0;
 }
